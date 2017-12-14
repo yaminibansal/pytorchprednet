efac138174d65ae7b74c7c69fbaf3aee0dda055f
yaminibansal <yamini63bansal@gmail.com>
2017-12-14 18:59:52 -0500

    Added code to save relevant quantities and evaluate validation loss

--- prednet/deterministic/prednet_main.py
+++ prednet/deterministic/prednet_main.py
@@ -16,6 +16,7 @@ from prednet.utils.plotting import plot_det_seq
 from prednet.utils.misc import timeSince
 from prednet.utils.data_utils import kittidata, mnist_co
 
+import os
 import time
 import math
 import hickle as hkl
@@ -28,6 +29,7 @@ import matplotlib.gridspec as gridspec
 ##########################################################
 
 parser = argparse.ArgumentParser(fromfile_prefix_chars='@')
+parser.add_argument('--randomseed', type=int, required=True, help='Give -1 to pick seed randomly')
 parser.add_argument('--dataset', required=True, help='kitti')
 parser.add_argument('--train_root', required=True, help='path to training data')
 parser.add_argument('--train_src_root', help='path to training data')
@@ -45,6 +47,7 @@ parser.add_argument('--hid_filt_size', type=int, nargs='*')
 parser.add_argument('--hid_ker_size', type=int, nargs='*')
 parser.add_argument('--dec_ker_size', type=int, nargs='*')
 parser.add_argument('--lr', type=float, default=0.001, help='Learning rate for optimizer Adam')
+parser.add_argument('--savedata', action='store_true', help='Save final model etc')
 parser.add_argument('--savepath', help='Path for saving the plots')
 parser.add_argument('--num_inp_plts', type=int, help='Number of input data sequences to be plotted')
 
@@ -55,6 +58,23 @@ print(opt)
 ######### Parse arguments and define variables ###########
 ##########################################################
 num_timesteps = opt.num_t
+
+if opt.randomseed==-1:
+    rseed = npr.randint(5000)
+else:
+    rseed = opt.randomseed
+npr.seed(rseed)
+if torch.cuda.is_available():
+    torch.cuda.manual_seed(rseed)
+else:
+    torch.manual_seed(rseed)
+
+# Making dir to save things if required
+if opt.savedata:
+    try:
+        os.makedirs(opt.savepath)
+    except OSError:
+        pass
 
 if opt.dataset == 'kitti':
     train_dataset = kittidata(opt.train_root, opt.train_src_root, num_timesteps, opt.samples_per_epoch)
@@ -102,28 +122,30 @@ for n in range(opt.num_epochs):
         if b % print_every == 0:
             print('Epoch %d: %s (%d %d%%) %.4f' % (n, timeSince(start), b, b / num_batches * 100, loss))
 
-# load data for validation loss
-i, val_input = enumerate(val_dataloader).next()
-val_input = Variable(val_input, requires_grad=False)
-if torch.cuda.is_available():
-    val_input = val_input.cuda()
-val_target = val_input
+    sum_val_loss = 0
+    for val_b, val_input in enumerate(val_dataloader):
+        val_input = Variable(val_input, requires_grad=False)
+        if torch.cuda.is_available():
+            val_input = val_input.cuda()
+        val_target = val_input
 
-val_output, val_loss = prednet_predict(model, criterion, val_input, val_target)
+        val_output, val_loss = prednet_predict(model, criterion, val_input, val_target)
+        sum_val_loss += val_loss
+    total_val_loss += [sum_val_loss/num_batches]
             
 ##########################################################
 ######################Store & Plot########################
 ##########################################################
 
-plt = plot_det_seq(val_input[:,:,0].data.cpu(), val_output[:,:,0].data.cpu(), opt.num_inp_plts, seq_ind=[2, 0], savepath=opt.savepath+'samples.png')
+plt = plot_det_seq(val_input[:,:,0].data.cpu(), val_output[:,:,0].data.cpu(), opt.num_inp_plts, seq_ind=[2, 0], savepath=opt.savepath+'/'+str(rseed)+'outputsamples.png')
 
-torch.save(model, opt.savepath+'model.pt')
+torch.save(model.state_dict(), opt.savepath+'/'+str(rseed)+'model.pt')
 
 output_dict = {}
-output_dict['output'] = val_output.data.cpu().numpy()
 output_dict['training loss'] = total_loss
-output_dict['validation loss'] = val_loss
+output_dict['validation loss'] = total_val_loss
+output_dict['seed']=rseed
 
-f = open(opt.savepath+'outdata.hkl', 'w')
+f = open(opt.savepath+'/'+str(rseed)+'outputs.pkl', 'w')
 hkl.dump(output_dict, f)
 f.close()
